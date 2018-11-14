6cf1813d550dcb19cadd6fa9093761360ad41cbb
Keresh <vuong@ninja.org>
2018-11-14 16:18:17 +0700

    Update route config

--- src/routes/routeWraper.js
+++ src/routes/routeWraper.js
@@ -3,14 +3,14 @@ import { Route } from 'react-router-dom';
 
 // wrap <Route> and use this everywhere instead, then when
 // sub routes are added to any route it'll work
-function RouteWithSubRoutes({ component: Component, routes, path, ...rest }) {
+function RouteWithSubRoutes({ component: Component, routes, path, componentProps, ...rest }) {
   return (
     <Route
       {...rest}
       path={path}
       render={props => (
         // pass the sub-routes down to keep nesting
-        <Component {...props} routes={routes} />
+        <Component {...props} {...componentProps} routes={routes} />
       )}
     />
   );
