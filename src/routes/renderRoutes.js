4134a0dfcc57065590824b7beca9d9ccb178fe19
Keresh <vuong@ninja.org>
2018-11-14 11:48:58 +0700

    Add privateRoute

--- src/routes/renderRoutes.js
+++ src/routes/renderRoutes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NotFoundPage from 'src/screens/notFound';
 import RouteWrapper from './routeWraper';
+import PrivateRoute from './privateRoute';
 
 export default ({ routes = [] }) => (
   <Switch>
-    { routes && routes.map(route => <RouteWrapper key={route.path} {...route} />) }
+    { routes && routes.map(route => (route.auth ? <PrivateRoute key={route.path} {...route} /> : <RouteWrapper key={route.path} {...route} />)) }
     <Route component={NotFoundPage} />
   </Switch>
 );
