6e8c6933b90656e2811311b38324b7770fc2d67e
0xmegalodon <dung.thanh@autonomous.nyc>
2018-11-15 17:52:12 +0700

    Update config

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,16 @@ builNumber=$(date "+%Y%m%d_%H%M%S")
 
 PROJECT="coin-exchange-221604"
 NAME_SPACE=$1
-NGINX_IMAGE="$NAME_SPACE-nginx"
 FRONTEND_IMAGE="$NAME_SPACE-frontend-service"
 
 gcloud auth activate-service-account --key-file deployments/deploy.cred.json
 gcloud container clusters get-credentials service-cluster-1 --zone asia-southeast1-a --project coin-exchange-221604
-docker build -t gcr.io/$PROJECT/$NGINX_IMAGE:$builNumber .
+docker build -t gcr.io/$PROJECT/$FRONTEND_IMAGE:$builNumber .
 
 printf '\033[31m%s\033[0m\n' "=== READY TO DEPLOY ======";
 
-docker tag gcr.io/$PROJECT/$NGINX_IMAGE:$builNumber gcr.io/$PROJECT/$NGINX_IMAGE:$builNumber
-gcloud docker -- push gcr.io/$PROJECT/$NGINX_IMAGE:$builNumber
-kubectl --namespace=$NAME_SPACE set image deployment/nginx nginx=gcr.io/$PROJECT/$NGINX_IMAGE:$builNumber
+docker tag gcr.io/$PROJECT/$FRONTEND_IMAGE:$builNumber gcr.io/$PROJECT/$FRONTEND_IMAGE:$builNumber
+gcloud docker -- push gcr.io/$PROJECT/$FRONTEND_IMAGE:$builNumber
+kubectl --namespace=$NAME_SPACE set image deployment/frontend-service frontend-service=gcr.io/$PROJECT/$FRONTEND_IMAGE:$builNumber
 
 printf '\033[32m%s\033[0m\n' "=== SUCCESSFULL DEPLOY $V ===== "
