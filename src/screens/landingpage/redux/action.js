e6bca71d44281d60c4aaee0869ec204cd6db543d
0xmegalodon <dung.thanh@autonomous.nyc>
2018-11-15 15:36:39 +0700

    Update component & request get FAQ content

--- src/screens/landingpage/redux/action.js
+++ src/screens/landingpage/redux/action.js
@@ -1,6 +1,6 @@
 import { makeRequest } from 'src/redux/action';
+import { API_URL } from 'src/resources/constants/url';
 import LANDINGPAGE_ACTION from './type';
-import { API_URL } from '@/constants';
 
 export const getFaqContent = () => makeRequest({
   type: LANDINGPAGE_ACTION.GET_FAQ_CONTENT,
