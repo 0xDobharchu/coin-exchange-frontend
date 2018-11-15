d493a864a8d4d4270a24a09849adad463460d374
Keresh <vuong@ninja.org>
2018-11-15 12:36:36 +0700

    Add zopim

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Root from 'src/screens/app/components/App/Root';
+import React, { Component } from 'react';
 import routes, { RenderRoutes } from 'src/routes';
+import zopim from 'src/services/zopim';
+import Root from 'src/screens/app/components/App/Root';
 import 'src/assets/styles/app.scss';
 
+class App extends Component {
+  componentDidMount() {
+    zopim.init();
+  }
 
-const App = () => (
-  <Root>
-    <RenderRoutes routes={routes} />
-  </Root>
-);
+  render() {
+    return (
+      <Root>
+        <RenderRoutes routes={routes} />
+      </Root>
+    );
+  }
+}
+
 export default App;
