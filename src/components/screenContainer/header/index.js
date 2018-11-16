8d590f0332b6d8ff116517d0553956812bd6ac8a
Keresh <vuong@ninja.org>
2018-11-16 17:07:41 +0700

    Add header

--- src/components/screenContainer/header/index.js
+++ src/components/screenContainer/header/index.js
@@ -1,9 +1,61 @@
 import React from 'react';
-import logo from 'src/assets/images/logo.png';
+import { Link } from 'react-router-dom';
+import logo from 'src/assets/images/logo.svg';
+import cx from 'classnames';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import styles from './styles.scss';
+
+const menus = {
+  about_us: {
+    name: 'About Us',
+    link: '/about-us',
+    icon: <MdKeyboardArrowDown />
+  },
+  faq: {
+    name: 'FAQ',
+    link: '/faq'
+  },
+  team: {
+    name: 'Team',
+    link: '/team'
+  },
+};
+
+const buttons = {
+  sign_in: {
+    name: 'Sign In',
+    link: '/sign-in',
+    className: 'buttonSignIn'
+  },
+  sign_up: {
+    name: 'Sign up',
+    link: '/sign-up',
+    className: 'buttonSignUp'
+  },
+};
 
 const Header = () => (
-  <header>
-    <img src={logo} alt="coinbowl-logo" />
+  <header className={styles.headerContainer}>
+    <img className={styles.logo} src={logo} alt="coinbowl-logo" />
+    <div className={styles.items}>
+      {
+        Object.entries(menus).map(([ key, menu ]) => (
+          <Link to={menu.link} key={key}>
+            <span className={styles.menuItem}>
+              {menu.name}
+              {menu.icon}
+            </span>
+          </Link>
+        ))
+      }
+    </div>
+    <div className={styles.buttons}>
+      {
+        Object.entries(buttons).map(([ key, button ]) => (
+          <Link to={button.link} key={key}><button type="button" className={cx(styles[button.className], styles.button)}>{button.name}</button></Link>
+        ))
+      }
+    </div>
   </header>
 );
 
--- src/routes/renderRoutes.js
+++ src/routes/renderRoutes.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NotFoundPage from 'src/screens/notFound';
+import ScreenContainer from 'src/components/screenContainer';
 import RouteWrapper from './routeWraper';
 import PrivateRoute from './privateRoute';
 
 export default ({ routes = [] }) => (
   <Switch>
-    { routes && routes.map(route => (route.auth ? <PrivateRoute key={route.path} {...route} /> : <RouteWrapper key={route.path} {...route} />)) }
+    { routes && routes.map(route => {
+      const Component = route.auth ? <PrivateRoute key={route.path} {...route} /> : <RouteWrapper key={route.path} {...route} />;
+      if (route.noContainer === true) {
+        return Component;
+      } else {
+        return (
+          <ScreenContainer>{Component}</ScreenContainer>
+        );
+      }
+    })}
     <Route component={NotFoundPage} />
   </Switch>
 );
