167cb37e5910110343b38853f9b570a1be3425d3
Keresh <vuong@ninja.org>
2018-11-16 11:33:03 +0700

    Update client config

---
+++ babel.config.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+  'presets' : [
+    [
+      '@babel/preset-env',
+      {
+        forceAllTransforms: true
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  'plugins': [
+    ['transform-imports', {
+      'react-bootstrap': {
+        'transform': 'react-bootstrap/lib/${member}',
+        'preventFullImport': true
+      },
+      'lodash': {
+        'transform': 'lodash/${member}',
+        'preventFullImport': true
+      },
+      'react-icons': {
+        'transform': 'react-icons/md/${member}',
+        'preventFullImport': true
+      }
+    }],
+    '@babel/plugin-syntax-dynamic-import',
+    'transform-function-bind',
+    ['transform-class-properties', { 'spec': true }],
+    '@babel/plugin-proposal-optional-chaining',
+    ['@babel/plugin-transform-runtime', {
+      'regenerator': true
+    }],
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-transform-react-jsx'
+  ]
+};
