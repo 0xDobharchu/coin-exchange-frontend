bb4dc04a35c5e630558de23a9b1a1b9fab47abf0
0xmegalodon <dung.thanh@autonomous.nyc>
2018-11-19 14:14:41 +0700

    -: update layout

--- src/screens/app/components/Layout/index.js
+++ src/screens/app/components/Layout/index.js
@@ -1,10 +1,20 @@
 import React, { PureComponent } from 'react';
 import cx from 'classnames';
-import Alert from '../../../../components/core/presentation/Alert';
-import Navigation from '../../../../components/core/controls/Navigation';
+import Alert from '@/components/core/presentation/Alert';
+import Navigation from '@/components/core/controls/Navigation';
 import styles from './styles.scss';
+import BrowserDetect from '@/services/browser-detect';
 
 class Layout extends PureComponent {
+  renderNavigation = (props) => {
+    const { isDesktop } = BrowserDetect;
+    const { name } = (window.name !== '' && JSON.parse(window.name));
+    if (isDesktop || name) return null;
+    return (
+      <Navigation location={props.location} />
+    );
+  }
+
   render() {
     console.log('Layout', this.props);
     return (
@@ -12,7 +22,7 @@ class Layout extends PureComponent {
         <div className={cx('common-fluid', styles.content)}>
           {this.props.children}
         </div>
-        <Navigation />
+        {this.renderNavigation(this.props)}
         <Alert />
       </React.Fragment>
     );
