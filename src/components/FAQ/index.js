424eb808909ae49eda3af3d503efcde6195d572d
0xmegalodon <dung.thanh@autonomous.nyc>
2018-11-19 09:11:53 +0700

    -: Update faq pages

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './styles.scss';
+import styles from './styles.scss';
 import Collapse from '@/components/Collapse';
+import { LabelLang } from '@/lang/components';
 
 class Faq extends React.PureComponent {
   componentDidMount() {
@@ -19,6 +20,9 @@ class Faq extends React.PureComponent {
         { faq && (
           <div className="row mt-5" id="faq" ref={(c) => { this.faq = c; }}>
             <div className="col">
+              <div className={styles.pdFaq}>
+                <LabelLang id="COIN_EXCHANGE_LP_FAQ_TITLE"/>
+              </div>
               <div>
                 {faq.map((item) => {
                   const { question, answer, order } = item;
