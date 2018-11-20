3df4a93e04cc4b6d91d95d7b15c1f91ca0938fc1
0xmegalodon <dung.thanh@autonomous.nyc>
2018-11-20 14:27:12 +0700

    -: update code get review

--- src/screens/coin/action.js
+++ src/screens/coin/action.js
@@ -1,11 +1,11 @@
 import { makeRequest } from 'src/redux/action';
-// import { API_URL } from 'src/resources/constants/url';
+import { API_URL } from 'src/resources/constants/url';
 import { GET_REVIEW, GET_BUY_PRICE, GET_SELL_PRICE } from './type';
 
-export const getReview = params => makeRequest({
+export const getReview = (data) => makeRequest({
   type: GET_REVIEW,
-  url: '',
-  params
+  url: `/${API_URL.EXCHANGE.REVIEWS}`,
+  params: data.params
 });
 
 export const coinGetSellPrice = () => makeRequest({
