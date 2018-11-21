a97c0fa767e3fb66bdeabd660475ed659d75c8d5
Keresh <vuong@ninja.org>
2018-11-21 21:57:46 +0700

    Add styles

--- src/screens/coin/index.js
+++ src/screens/coin/index.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
+import cx from 'classnames';
+import { Container, Col, Row } from 'react-bootstrap';
 import BuyCoin from './buy';
-// import styles from './styles.scss';
+import styles from './styles.scss';
 
-const Coin = () => (
-  <React.Fragment>
-    <div className='container'>
-      <BuyCoin />
-    </div>
-  </React.Fragment>
-);
+const TABS = {
+  BUY: {
+    title: 'BUY COIN',
+    component: <BuyCoin />
+  },
+  SELL: {
+    title: 'SELL COIN',
+    component: <span>SELL COIN</span>
+  }
+};
+
+class Coin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeTab: Object.keys(TABS)[0],
+    };
+  }
+
+  onSelectTab = (key) => {
+    this.setState({ activeTab: key });
+  }
+
+  renderTabHeader = () => {
+    const { activeTab } = this.state;
+    return Object.entries(TABS).map(([key, tab]) => (
+      <div
+        key={key}
+        className={cx(styles.tabTitle, key === activeTab ? styles.headerActive : '')}
+        role='presentation'
+        onClick={()=> this.onSelectTab(key)}
+      >
+        {tab.title}
+      </div>
+    ));
+  }
+
+  renderTabContent = () => {
+    const { activeTab } = this.state;
+    return Object.entries(TABS).map(([key, tab]) => (
+      <div key={key} className={cx(styles.tabContent, key === activeTab ? styles.active : styles.hidden)}>
+        {tab.component}
+      </div>
+    ));
+  }
+
+  render() {
+    return (
+      <Container className={styles.container}>
+        <Row>
+          <Col lg={3}>
+            <div className={styles.panelLeft} />
+          </Col>
+          <Col lg={6}>
+            <div className={styles.main}>
+              <div className={styles.header}>
+                {this.renderTabHeader()}
+              </div>
+              <div className={styles.content}>
+                {this.renderTabContent()}
+              </div>
+            </div>
+          </Col>
+          <Col lg={3}>
+            <div className={styles.panelRight} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default Coin;
