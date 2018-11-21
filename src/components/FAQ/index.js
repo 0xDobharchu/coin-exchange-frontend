ffb0a2e5e979ce16014e9f6e9d5aa791d495609a
blackbean <tuananh@autonomous.nyc>
2018-11-21 17:34:18 +0700

    fixed build server staging

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LabelLang from 'src/lang/components/LabelLang';
+import Collapse from 'src/components/Collapse';
 import styles from './styles.scss';
-import Collapse from '@/components/Collapse';
 
 class Faq extends React.PureComponent {
   componentDidMount() {
