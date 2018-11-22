98b87d31183342f2df2514ce6f065870ee0415d1
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-11-22 16:23:58 +0700

    Change Password Success

--- src/screens/me/pages/AccountInfo/ChangePassword/index.js
+++ src/screens/me/pages/AccountInfo/ChangePassword/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Input, Button } from '@/components/custom';
+import { Field, reduxForm }from 'redux-form';
+import { InputField, Button } from '@/components/custom';
 import {  Row, Col }from 'react-bootstrap';
 import style from './style.scss';
 
-const ChangePassword = () => (
+// eslint-disable-next-line
+const ChangePasswordForm = ({ handleSubmit, onSubmit }) => (
   <Row className={style.container}>
-    <Col md={4}><Input type="password" placeholder="Old password" /></Col>
-    <Col md={4}><Input type="password" placeholder="New password" /></Col>
-    <Col md={4}><Button value="Change password" /></Col>
+    <Col md={4}><Field name="old_password" component={InputField} placeholder="Old Password" /></Col>
+    <Col md={4}><Field name="password" component={InputField} placeholder="New Password" /></Col>
+    <Col md={4}><Button value="Change password" onClick={handleSubmit(onSubmit)} /></Col>
   </Row>
 );
 
-export default ChangePassword;
+export default reduxForm({
+  form: 'ChangePasswordForm',
+})(ChangePasswordForm);
