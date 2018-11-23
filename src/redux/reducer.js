f330d8f20183bb98caf8bea41b722a639d12bb3f
Keresh <vuong@ninja.org>
2018-11-23 17:12:55 +0700

    Fixed - missing reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,7 +4,9 @@ import auth from 'src/screens/auth/redux/reducer';
 import homeReducer from 'src/screens/home/reducer';
 import reviewListReducer from 'src/components/reviewList/reducer';
 import landingReducer from 'src/screens/landingpage/redux/reducer';
+import screenCoinReducer from 'src/screens/coin/reducer';
 import langReducer from '../lang/reducer';
+
 // eslint-disable-next-line
 import { reducer as form } from 'redux-form';
 
@@ -15,6 +17,7 @@ const reducers = {
   langReducer,
   reviewListReducer,
   landingReducer,
+  screenCoinReducer,
   form,
 };
 
