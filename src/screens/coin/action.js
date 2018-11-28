b3562bb2a3aa8b8de70f1eec437fade2b6016f76
blackbean <tuananh@autonomous.nyc>
2018-11-28 13:54:11 +0700

    add Refferal when sign-up

--- src/screens/coin/action.js
+++ src/screens/coin/action.js
@@ -1,12 +1,6 @@
 import { makeRequest } from 'src/redux/action';
 import { API_URL } from 'src/resources/constants/url';
-import { GET_REVIEW, GET_BUY_PRICE, GET_SELL_PRICE } from './type';
-
-export const getReview = (data) => makeRequest({
-  type: GET_REVIEW,
-  url: `/${API_URL.EXCHANGE.REVIEWS}`,
-  params: data.params
-});
+import { GET_BUY_PRICE, GET_SELL_PRICE } from './type';
 
 export const coinGetSellPrice = (data) => makeRequest({
   type: GET_SELL_PRICE,
