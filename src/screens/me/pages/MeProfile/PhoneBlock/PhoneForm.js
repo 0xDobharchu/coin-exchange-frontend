366a92cc38f6084d90c87f04851d42a958d72cbd
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-11-28 18:08:08 +0700

    Phonenumber disable

--- src/screens/me/pages/MeProfile/PhoneBlock/PhoneForm.js
+++ src/screens/me/pages/MeProfile/PhoneBlock/PhoneForm.js
@@ -6,11 +6,11 @@ import { Row } from 'react-bootstrap';
 // import { FieldLang } from 'src/lang/components';
 import { MyMessage, FieldLang } from 'src/lang/components';
 import ReactPhoneInput from 'react-phone-input-2';
-
+import { getReachingLevel } from '../util';
 import style from '../styles.scss';
 
-const renderFieldPhone = ({ input }) => (
-  <ReactPhoneInput {...input} defaultCountry='hk' regions='asia' placeholder="me.profile.text.phone.desc3" inputStyle={{ width: '100%' }} />
+const renderFieldPhone = ({ input, disabled }) => (
+  <ReactPhoneInput {...input} defaultCountry='hk' regions='asia' placeholder="me.profile.text.phone.desc3" inputStyle={{ width: '100%' }} disabled={disabled} />
 );
 
 // eslint-disable-next-line
@@ -25,7 +25,7 @@ const PhoneForm = ({ handleSubmit, onSubmit, level, levelStatus ,phoneNumber, ch
       <Row>
         <div className="col-10">
           {/* <ReactPhoneInput name='phone' value={phoneNumber} defaultCountry='hk' onChange={handleOnChange} regions='asia' placeholder="me.profile.text.phone.desc3" inputStyle={{ width: '100%' }} disabled={level === 'level_2' || level === 'level_3' || level === 'level_4'} /> */}
-          <FieldLang name="phone" component={renderFieldPhone} />
+          <FieldLang name="phone" component={renderFieldPhone} disabled={getReachingLevel(level, levelStatus) >= 2} />
         </div>
         {level === 'level_2' && levelStatus === 'pending' && <div className="col-10" style={{ width: '100%' }}><FieldLang style={{ width: '100%' }} name="code" component="input" type="text" placeholder="me.profile.text.phone.desc3" /></div>}
         <div className="col-2" style={{ paddingLeft: 0 }}>
@@ -50,8 +50,5 @@ export default compose(
   connect(mapState),
   reduxForm({
     form: 'PhoneForm',
-    // validate,
-    // warn,
-    // enableReinitialize: true,
   })
 )(PhoneForm);
