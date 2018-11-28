a2407aadf9edc5fa3ef969505f9af62187b81631
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-11-28 17:27:00 +0700

    Phone Change Example

--- src/screens/me/pages/MeProfile/PhoneBlock/PhoneForm.js
+++ src/screens/me/pages/MeProfile/PhoneBlock/PhoneForm.js
@@ -9,26 +9,35 @@ import ReactPhoneInput from 'react-phone-input-2';
 
 import style from '../styles.scss';
 
-// eslint-disable-next-line
-const PhoneForm = ({ handleSubmit, onSubmit, level, levelStatus ,phoneNumber}) => (
-  <div>
-    <Row>
-      <div className="col-10">
-        <ReactPhoneInput value={phoneNumber} defaultCountry='hk' onChange={handleOnChange} regions='asia' placeholder="me.profile.text.phone.desc3" inputStyle={{ width: '100%' }} disabled={level === 'level_2' || level === 'level_3' || level === 'level_4'} />
-      </div>
-      {level === 'level_2' && levelStatus === 'pending' && <div className="col-10" style={{ width: '100%' }}><FieldLang style={{ width: '100%' }} name="code" component="input" type="text" placeholder="me.profile.text.phone.desc3" /></div>}
-      <div className="col-2" style={{ paddingLeft: 0 }}>
-        <button onClick={handleSubmit(onSubmit)} type="button" className={style.submit_btn}>
-          <MyMessage id="me.profile.text.email.button.send" />
-        </button>
-      </div>
-    </Row>
-  </div>
+const renderFieldPhone = ({ input }) => (
+  <ReactPhoneInput {...input} defaultCountry='hk' regions='asia' placeholder="me.profile.text.phone.desc3" inputStyle={{ width: '100%' }} />
 );
 
-function handleOnChange(value) {
-  console.log(value);
-}
+// eslint-disable-next-line
+const PhoneForm = ({ handleSubmit, onSubmit, level, levelStatus ,phoneNumber, change }) => {
+  // const handleOnChange = (values) => {
+  //   change('phone', values);
+  //   console.log(values);
+  // };
+
+  return (
+    <div>
+      <Row>
+        <div className="col-10">
+          {/* <ReactPhoneInput name='phone' value={phoneNumber} defaultCountry='hk' onChange={handleOnChange} regions='asia' placeholder="me.profile.text.phone.desc3" inputStyle={{ width: '100%' }} disabled={level === 'level_2' || level === 'level_3' || level === 'level_4'} /> */}
+          <FieldLang name="phone" component={renderFieldPhone} />
+        </div>
+        {level === 'level_2' && levelStatus === 'pending' && <div className="col-10" style={{ width: '100%' }}><FieldLang style={{ width: '100%' }} name="code" component="input" type="text" placeholder="me.profile.text.phone.desc3" /></div>}
+        <div className="col-2" style={{ paddingLeft: 0 }}>
+          <button onClick={handleSubmit(onSubmit)} type="button" className={style.submit_btn}>
+            <MyMessage id="me.profile.text.email.button.send" />
+          </button>
+        </div>
+      </Row>
+    </div>
+  );
+};
+
 const mapState = state => ({
   initialValues: {
     phone: state.auth.profile.phone_number,
