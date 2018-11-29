5a941049907562dc7a0021a6d4480802e1b2f67e
0xmegalodon <dung.thanh@autonomous.nyc>
2018-11-29 17:16:09 +0700

    -: fix load localize en if some language not have value

--- src/screens/about/index.js
+++ src/screens/about/index.js
@@ -44,7 +44,7 @@ class About extends React.Component {
 }
 
 const mapState = state => ({
-  locale: state ?.app.locale || 'en',
+  locale: state.langReducer.lang || 'en'
 });
 
 const mapDispatch = dispatch => ({
