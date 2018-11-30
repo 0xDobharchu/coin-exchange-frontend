7df3ead5a123a7c65df748c89961f43027e37dfd
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-11-30 13:32:37 +0700

    fixbugs

--- src/screens/me/pages/MeProfile/PhoneBlock/PhoneForm.js
+++ src/screens/me/pages/MeProfile/PhoneBlock/PhoneForm.js
@@ -8,7 +8,7 @@ import { LabelLang, FieldLang } from 'src/lang/components';
 // import ReactPhoneInput from 'react-phone-input-2';
 
 import style from '../styles.scss';
-import {getReachingLevel} from '../util';
+import { getReachingLevel, getCurrentLevel  } from '../util';
 
 // const renderFieldPhone = ({ input }) => (
 //   <ReactPhoneInput {...input} defaultCountry='hk' regions='asia' placeholder="me.profile.text.phone.desc3" inputStyle={{ width: '100%' }} />
@@ -31,9 +31,9 @@ const PhoneForm = ({ handleSubmit, onSubmit, level, levelStatus ,phoneNumber, ch
           {<CountryPhone name='phone' defaultCountry='hk' regions='asia' inputStyle={{ width: '100%' }} disabled={getReachingLevel(level, levelStatus) >= 2} /> }
 
         </div>
-        {level === 'level_2' && levelStatus === 'pending' && <div className="col-10" style={{ width: '100%' }}><FieldLang style={{ width: '100%' }} name="code" component="input" type="text" placeholder="me.profile.text.phone.desc3" /></div>}
+        {level === 'level_2' && levelStatus === 'pending' && <div className="col-10" style={{ width: '100%', marginTop: '20px' }}><FieldLang style={{ width: '100%' }} name="code" component="input" type="text" placeholder="me.accountLevel.phoneCode" /></div>}
         <div className="col-2" style={{ paddingLeft: 0 }}>
-          <button onClick={handleSubmit(onSubmit)} type="button" className={style.submit_btn} disabled={getReachingLevel(level, levelStatus) >= 2}>
+          <button onClick={handleSubmit(onSubmit)} type="button" className={style.submit_btn} disabled={getCurrentLevel(level, levelStatus) >= 2}>
             <LabelLang id="me.accountLevel.ok" />
           </button>
         </div>
@@ -44,7 +44,7 @@ const PhoneForm = ({ handleSubmit, onSubmit, level, levelStatus ,phoneNumber, ch
 
 const mapState = state => ({
   initialValues: {
-    phone: state.auth.profile.phone_number,
+    phone: state.auth.profile.phone_number || state.auth.profile.pending_phone_number,
   },
   phoneNumber: state.auth.profile.phone_number,
   level: state.auth.profile.verification_level,
