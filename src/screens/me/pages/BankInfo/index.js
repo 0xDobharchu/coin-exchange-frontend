96761aab89a50994d61fef82d498f3ba334fec06
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-12-03 16:03:31 +0700

    Localization

--- src/screens/me/pages/BankInfo/index.js
+++ src/screens/me/pages/BankInfo/index.js
@@ -14,13 +14,14 @@ const BankInfo = ({ updateProfileAction, showAlert }) => {
   const handleSubmit = values => {
     updateProfileAction({ payment_info: JSON.stringify(values) }).then(() => showSuccess('me.bankInfo.alert.success')).catch(() => showError('me.bankInfo.alert.failed'));
   };
+  const handleDelete = () => updateProfileAction({ payment_info: '' }).then(() => showSuccess('me.bankInfo.alert.success')).catch(() => showError('me.bankInfo.alert.failed'));
 
   return (
     <div className={style.container}>
       <label className={style.title}><LabelLang id="me.bankInfo.title" /></label>
       <div className={style.lineTitle} />
       <div className={style.block1}>
-        <BankInfoForm isEditMode={false} onSubmit={handleSubmit} onDelete={() => console.log('goign to delete bak info')} />
+        <BankInfoForm isEditMode={false} onSubmit={handleSubmit} onDelete={handleDelete} />
       </div>
     </div>);
 };
