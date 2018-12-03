355afd97ba08d25826c7c36c171a27a3524a7cae
0xngoubou <phuong@autonomous.nyc>
2018-12-03 16:21:07 +0700

    update css + fix submit contact with phone number

--- src/screens/contact/action.js
+++ src/screens/contact/action.js
@@ -2,14 +2,14 @@ import { makeRequest } from 'src/redux/action';
 import { API_URL } from 'src/resources/constants/url';
 import {ADD_CONTACT} from './type';
 
-function addContact(name, phone, email, description) {
+function addContact(name, phone_number, email, description) {
   return makeRequest({
     type: ADD_CONTACT,
     url: API_URL.SYSTEM.SUBMIT_CONTACT,
     method: 'POST',
     withAuth: false,
     data: {
-      name, phone, email, description
+      name, phone_number, email, description
     }
   });
 }
