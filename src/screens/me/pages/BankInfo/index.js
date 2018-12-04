ad453e0492becd2d547a0ea242db4f41aff277ad
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-12-04 16:42:04 +0700

    Add Confirm Dialog

--- src/screens/me/pages/BankInfo/index.js
+++ src/screens/me/pages/BankInfo/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { LabelLang } from 'src/lang/components';
 import { showAlert } from 'src/screens/app/redux/action';
+import ConfirmDialog from 'src/components/confirmDialog';
 import { updateProfileAction } from 'src/screens/auth/redux/action';
 import BankInfoForm from './form/BankInfoForm';
 import style from './style.scss';
@@ -12,21 +13,27 @@ class BankInfo extends React.Component {
   constructor(props) {
     super(props);
     this.bankInfoForm = React.createRef();
+    this.confirmDialogUpdate = React.createRef();
+    this.confirmDialogDelete = React.createRef();
   }
   // eslint-disable-next-line
   showMessage = (message, type) => this.props.showAlert({ message, type });
   showSuccess = id => this.showMessage(id, 'success');
   showError = id => this.showMessage(id, 'danger');
   handleSubmit = values => {
+    this.confirmDialogUpdate.current.show(values);
+  };
+  onConfirmUpdate = values => {
     //eslint-disable-next-line
     this.props.updateProfileAction({ payment_info: JSON.stringify(values) }).then(() => {
       this.bankInfoForm.current.getWrappedInstance().ref.getWrappedInstance().wrapped.switchViewMode();
       this.showSuccess('me.bankInfo.alert.success'); 
     }).catch(() => this.showError('me.bankInfo.alert.failed'));
-  };
+  }
 
+  handleDelete = () => this.confirmDialogDelete.current.show();;
   // eslint-disable-next-line
-  handleDelete = () => this.props.updateProfileAction({ payment_info: '' }).then(() => {
+  onConfirmDelete = () => this.props.updateProfileAction({ payment_info: '' }).then(() => {
     this.bankInfoForm.current.getWrappedInstance().ref.getWrappedInstance().wrapped.toggleEdit();
     this.showSuccess('me.bankInfo.alert.deleteSuccess'); 
   }).catch(() => this.showError('me.bankInfo.alert.deleteFailed'));
@@ -40,6 +47,22 @@ class BankInfo extends React.Component {
         <div className={style.block1}>
           <BankInfoForm ref={this.bankInfoForm} isEditMode={false} onSubmit={this.handleSubmit} onDelete={this.handleDelete} />
         </div>
+        <ConfirmDialog
+          title={<LabelLang id="me.bankInfo.dialog.update.title" />}
+          body={<LabelLang id="me.bankInfo.dialog.update.body" />}
+          confirmText={<LabelLang id="me.bankInfo.dialog.update.confirm" />}
+          cancelText={<LabelLang id="me.bankInfo.dialog.update.cancel" />}
+          ref={this.confirmDialogUpdate}
+          onConfirm={this.onConfirmUpdate}
+        />
+        <ConfirmDialog
+          title={<LabelLang id="me.bankInfo.dialog.delete.title" />}
+          body={<LabelLang id="me.bankInfo.dialog.delete.body" />}
+          confirmText={<LabelLang id="me.bankInfo.dialog.delete.confirm" />}
+          cancelText={<LabelLang id="me.bankInfo.dialog.delete.cancel" />}
+          ref={this.confirmDialogDelete}
+          onConfirm={this.onConfirmDelete}
+        />
       </div>
     );
   }
