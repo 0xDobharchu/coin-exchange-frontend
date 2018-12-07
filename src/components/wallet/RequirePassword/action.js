e074fb4fbef4ba90885ba0faa88fd1e8b468f330
0xngoubou <phuong@autonomous.nyc>
2018-12-07 18:11:28 +0700

    update confirm require password

---
+++ src/components/wallet/RequirePassword/action.js
@@ -0,0 +1,22 @@
+import { makeRequest } from 'src/redux/action';
+import { API_URL } from 'src/resources/constants/url';
+import { VERYFY_PASSWORD } from './type';
+
+export const vefiryPassword = (password) => (dispatch) => {
+    const makeVefiryPassword = makeRequest({
+        type: VERYFY_PASSWORD,
+        url: API_URL.USER.USER_VERYFY_PASSWORD,
+        method: 'POST',
+        data: { password }
+    }, dispatch);
+
+    return makeVefiryPassword().then((res) => {
+        console.log(res);
+        return res === "true"   
+    }, (err) => {
+        console.log(err);
+        return false;
+    });
+};
+
+export default vefiryPassword;
