dd25daab83c2cfe0fbde59fc6d2f3ac4a3691a80
0xmegalodon <dung.thanh@autonomous.nyc>
2018-12-12 11:56:57 +0700

    -: update layout promotion program page

--- src/screens/promotionProgram/index.js
+++ src/screens/promotionProgram/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import LabelLang from 'src/lang/components/LabelLang';
-import earlyBirdIcon from 'src/assets/images/promotionPrograms/early_bird_program.svg';
 import referralIcon from 'src/assets/images/promotionPrograms/referral_program.svg';
-import referrerIcon from 'src/assets/images/promotionPrograms/referrer_icon.svg';
-import userIcon from 'src/assets/images/promotionPrograms/user_icon.svg';
 import ProgramItem from 'src/screens/promotionProgram/programItem';
 import styles from './styles.scss';
 
 const programs = [
-  {
-    logo: earlyBirdIcon,
-    title: <LabelLang id='promotion_programs.early_bird_program.title' />,
-    description: <LabelLang id='promotion_programs.early_bird_program.description' />,
-    key: 'earlyBird'
-  },
+  // {
+  //   logo: earlyBirdIcon,
+  //   title: <LabelLang id='promotion_programs.early_bird_program.title' />,
+  //   description: <LabelLang id='promotion_programs.early_bird_program.description' />,
+  //   key: 'earlyBird'
+  // },
   {
     logo: referralIcon,
     title: <LabelLang id='promotion_programs.referral_program.title' />,
@@ -23,51 +20,40 @@ const programs = [
   },
 ];
 
-const numEarlyBirdTermAndCondition = 4;
-let earlyBirdTermAndCondition = [];
-for (let i = 0; i < numEarlyBirdTermAndCondition; i++) {
-  earlyBirdTermAndCondition.push(<LabelLang id={`promotion_programs.early_bird_program.termAndConditions.${i}`} />);
+const numBenefits = 2;
+const benefits = [];
+
+for (let i = 0; i < numBenefits; i++) {
+  benefits.push(<LabelLang id={`promotion_programs.referral_program.benefits.content.${i}`} />);
 }
 
-const numRefferTermAndCondition = 3;
-const referrerReferralTermAndCondition = [];
+const numHowToDo = 1;
+const howToDo = [];
 
-for (let i = 0; i < numRefferTermAndCondition; i++) {
-  referrerReferralTermAndCondition.push(<LabelLang id={`promotion_programs.referral_program.referrer.termAndConditions.${i}`} />);
+for (let i = 0; i < numHowToDo; i++) {
+  howToDo.push(<LabelLang id={`promotion_programs.referral_program.howToDo.content.${i}`} />);
 }
 
-const numRefferDescription = 2;
-const referrerDescription = [];
-for (let i = 0; i < numRefferDescription; i++) {
-  referrerDescription.push(<LabelLang id={`promotion_programs.referral_program.referrer.descriptions.${i}`} />);
-}
-
-const numUserTermAndCondition = 2;
-const userReferralTermAndCondition = [];
-
-for (let i = 0; i < numUserTermAndCondition; i++) {
-  userReferralTermAndCondition.push(<LabelLang id={`promotion_programs.referral_program.user.termAndConditions.${i}`} />);
-}
+const referralContent = [
+  {
+    title: <LabelLang id="promotion_programs.referral_program.benefits.title" />,
+    content: benefits
+  },
+  {
+    title: <LabelLang id="promotion_programs.referral_program.howToDo.title" />,
+    content: howToDo
+  }
+];
 
 class PromotionProgram extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      programType: 'earlyBird',
-    };
   }
 
   componentDidMount() {
   }
 
-  choosePromotionProgram = (programType) => {
-    console.log('choosePromotionProgram', programType);
-    this.setState({ programType });
-  }
-
   render() {
-    const { programType } = this.state;
-    console.log('userTermAndCondition',earlyBirdTermAndCondition);
     let index = 0;
     return (
       <div className={styles.container}>
@@ -79,74 +65,34 @@ class PromotionProgram extends React.Component {
             const { logo, title, description, key } = item;
             index++;
             return (
-              <ProgramItem logo={logo} title={title} description={description} key={key} programType={key} choosePromotionProgram={this.choosePromotionProgram} index={index} />
+              <ProgramItem logo={logo} title={title} description={description} key={key} programType={key} index={index} />
             );
           })}
         </div>
         <div className={styles.content}>
-          {programType === 'earlyBird' ?
-            (
-              <div>
-                <div className={styles.termAndCondition}>
-                  <LabelLang id="promotion_programs.termAndConditions" />
-                  <hr />
-                </div>
-                {
-                  earlyBirdTermAndCondition.map(item => {
-                    index++;
-                    return <p className={styles.text} key={index}>{item}</p>;
-                  })
-                }
-              </div>
-            ):
-            (
-              <div>
-                <div>
-                  <div className={styles.userHeader}>
-                    <div className={styles.userTitle}>
-                      <img src={referrerIcon} alt="" />
-                      <div className={styles.userTitleName}><LabelLang id="promotion_programs.referral_program.referrer.title" /></div>
-                    </div>
-                    <LabelLang id="promotion_programs.referral_program.referrer.description" className={styles.userTitleContent} />
-                  </div>
+          {
+            referralContent.map(item => {
+              index++;
+              const {title, content} = item;
+              index++;
+              return (
+                <div key={index}>
                   <div className={styles.termAndCondition}>
-                    <LabelLang id="promotion_programs.termAndConditions" />
+                    {title}
                     <hr />
                   </div>
-                  {
-                  referrerReferralTermAndCondition.map(item => {
-                    index++;
-                    return <p className={styles.text} key={index}>{item}</p>;
-                  })
-                }
+                  <ul>
+                    {
+                      content.map(ct => {
+                        index++;
+                        return (<li key={index}>{ct}</li>);
+                      })
+                    }
+                  </ul>
                 </div>
-                <div>
-                  <div className={styles.userHeader}>
-                    <div className={styles.userTitle}>
-                      <img src={userIcon} alt="" />
-                      <div className={styles.userTitleName}><LabelLang id="promotion_programs.referral_program.user.title" /></div>
-                    </div>
-                    <LabelLang id="promotion_programs.referral_program.user.description" className={styles.userTitleContent} />
-                  </div>
-                  {
-                    referrerDescription.map(item => {
-                      index++;
-                      return <p className={styles.text} key={index}>{item}</p>;
-                    })
-                  }
-                  <div className={styles.termAndCondition}>
-                    <LabelLang id="promotion_programs.termAndConditions" />
-                    <hr />
-                  </div>
-                  {
-                    userReferralTermAndCondition.map(item => {
-                      index++;
-                      return <p className={styles.text} key={index}>{item}</p>;
-                    })
-                  }
-                </div>
-              </div>
-            )}
+              );
+            })
+          }
         </div>
       </div>
     );
