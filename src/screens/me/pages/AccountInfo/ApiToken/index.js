cb35fc7b263006b9d8d2acc1de3c05b9a54e5e65
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-12-17 08:41:39 +0700

    ApiToken

--- src/screens/me/pages/AccountInfo/ApiToken/index.js
+++ src/screens/me/pages/AccountInfo/ApiToken/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Loading from 'src/components/loading';
 import { getApiTokenAction, deleteApiTokenAction, updateApiTokenAction } from 'src/screens/auth/redux/action';
 import { Button } from 'src/components/custom';
 import { connect } from 'react-redux';
+import {showAlert} from 'src/screens/app/redux/action';
 import { WrapperLang } from 'src/lang/components';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import style from './styles.scss';
 
 class ApiToken extends React.Component {
@@ -18,18 +21,24 @@ class ApiToken extends React.Component {
   // eslint-disable-next-line
   handleUpdate = () => this.props.updateApiTokenAction();
 
+  // eslint-disable-next-line
+  showMessage = (message, type) => this.props.showAlert({ message, type });
+  showSuccess = id => this.showMessage(id, 'success');
+  handleOnClickCopy = () => this.showSuccess('me.accountInfo.alert.apiTokenCopyLinkSuccess');
   render() {
     const { loading }= this.state;
     // eslint-disable-next-line
     const { tokenApi } = this.props;
-    if (loading) return <div>loading...</div>;
+    if (loading) return <Loading />;
     return (
       <WrapperLang>
         {ts => (
           <div className={style.container}>
             {tokenApi && <div className={style.token}>{tokenApi}</div>}
-            {tokenApi && <Button type="button" onClick={this.handleDelete} value={ts('me.accountInfo.apiTokenDelete')} />}
-            {!tokenApi && <Button type="button" onClick={this.handleUpdate} value={ts('me.accountInfo.apiTokenNew')} />}
+            <CopyToClipboard text={tokenApi} onCopy={this.handleOnClickCopy}>
+              <Button value={ts('me.accountInfo.apiTokenCopy')} />
+            </CopyToClipboard>
+            <Button type="button" onClick={this.handleUpdate} value={ts('me.accountInfo.apiTokenNew')} />
           </div>
         )}
       </WrapperLang>
@@ -39,5 +48,5 @@ class ApiToken extends React.Component {
 const mapState = state => ({
   tokenApi: state.auth.tokenApi
 });
-const mapDispatch = { getApiTokenAction, deleteApiTokenAction, updateApiTokenAction };
+const mapDispatch = { getApiTokenAction, deleteApiTokenAction, updateApiTokenAction, showAlert };
 export default connect(mapState, mapDispatch)(ApiToken);
