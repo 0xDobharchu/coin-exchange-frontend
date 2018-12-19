0f998228cdc1bae87068fc9f58c7fa2ae72e79fc
Keresh <vuong@ninja.org>
2018-12-19 17:34:07 +0700

    Update PWA manifest

--- src/client.js
+++ src/client.js
@@ -5,14 +5,32 @@ import App from 'src/app';
 import { Provider } from 'react-redux';
 import configureStore from 'src/redux/store';
 
-/** Temporary disbabled */
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('SW registered: ', registration);
-    }).catch((registrationError) => {
-      console.log('SW registration failed: ', registrationError);
+if (__CLIENT__) {
+  // Service worker register, implement workbox
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then((registration) => {
+        console.log('SW registered: ', registration);
+      }).catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
     });
+  }
+
+  // listen "Add to home screen" event
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    // Stash the event so it can be triggered later.
+    e.prompt();
+    // Wait for the user to respond to the prompt
+    e.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          alert('Nice! Now you can access Coinbowl via home screen');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+      });
   });
 }
 
