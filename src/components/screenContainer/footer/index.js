81f2bdb80995459881bffe54eef1fd80aa0a8010
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-12-21 11:42:47 +0700

    New Design Footer

--- src/components/screenContainer/footer/index.js
+++ src/components/screenContainer/footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WrapperLang } from 'src/lang/components';
+import { WrapperLang, LabelLang } from 'src/lang/components';
 import SocialBox from 'src/screens/me/pages/Referral/socialBox';
 import style from './styles.scss';
 
@@ -7,13 +7,26 @@ const Footer = () => (
   <footer className={style.container}>
     <div className={style.footer}>
       <div className={style.col}>
-        <div className={style.desc}>21 Revolution Street</div>
-        <div className={style.desc}>+1 555 123456</div>
-        <div className={style.bold}>support@company.com</div>
+        <div className={style.bold}>
+          <LabelLang id="footer.privacy" />
+        </div>
+        <div className={style.bold}>
+          <LabelLang id="footer.referralProgram" />
+        </div>
+        <div className={style.bold}>
+          <LabelLang id="footer.assetIntro" />
+        </div>
+        <div className={style.bold}>
+          <LabelLang id="footer.apiDocumentation" />
+        </div>
       </div>
       <div className={style.col}>
-        <div className={style.title}>About CoinBowl</div>
-        <div className={style.desc}>Coinbowl.com is a new crypto exchange specifically designed for people living in Asia, who have had limited access to crypto markets. CoinBowl enables users to trade large amounts of crypto at great prices. Users can buy or sell an uncapped quantity of crypto with 1 price for 1 order</div>
+        <div className={style.title}>
+          <LabelLang id="footer.aboutTitle" />
+        </div>
+        <div className={style.desc}>
+          <LabelLang id="footer.aboutDesc" />
+        </div>
         <div className={style.socialBox}>
           <WrapperLang>
             {ts =>
